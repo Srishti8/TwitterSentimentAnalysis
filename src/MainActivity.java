f1794518deb9a7c43c711fc22440f3c553b698ae
SJR1 <srishti.jhanwar84@gmail.com>
2016-05-25 14:14:11 +0530

    Sentiment count added

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -10,9 +10,20 @@ public class MainActivity {
         String topic="DigitalKranti";
         ArrayList<String> tweets = TweetManager.getTweets(topic);
         NLP.Init();
+        int sentimentCount1 = 0,sentimentCount2=0,sentimentCount3=0;
         for (String tweet : tweets)
         {
             System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+            if(NLP.findSentiment(tweet) == 1)
+                sentimentCount1++;
+            else if(NLP.findSentiment(tweet) == 2 )
+                sentimentCount2++;
+            else if(NLP.findSentiment(tweet) == 3)
+                sentimentCount3++;
         }
+
+        System.out.println("Tweets with score 1 : " + sentimentCount1);
+        System.out.println("Tweets with score 2 : " + sentimentCount2);
+        System.out.println("Tweets with score 3 : " + sentimentCount3);
     }
 }
