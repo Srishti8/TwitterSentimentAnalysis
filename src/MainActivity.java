02c773d2e5140a1c41a1da6a38e29c53e90789c1
SJR1 <srishti.jhanwar84@gmail.com>
2016-05-27 16:47:16 +0530

    More Changes

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -4,6 +4,9 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
@@ -12,9 +15,11 @@ import java.util.ArrayList;
  */
 public class MainActivity extends JFrame{
 
+    static JFrame frame = new JFrame();
+    private JPanel panel;
     public static void main(String[] args)
     {
-        String topic="NaMo";
+        String topic="Kohli";
         ArrayList<String> tweets = TweetManager.getTweets(topic);
         NLP.Init();
         int sentimentCount1 = 0,sentimentCount2=0,sentimentCount3=0;
@@ -39,10 +44,25 @@ public class MainActivity extends JFrame{
         pieDataset.setValue("Euphoric",sentimentCount3);
 
         JFreeChart chart = ChartFactory.createPieChart("Twitter Sentiment Analysis",pieDataset);
-        ChartPanel chartPanel = new ChartPanel(chart);
 
         BufferedImage image = chart.createBufferedImage(500,300);
         JLabel labelChart = new JLabel();
         labelChart.setIcon(labelChart.getIcon());
+
+        frame.setSize(500,500);
+        frame.setLayout(new GridLayout(3,1));
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+            }
+        });
+
+        frame.setContentPane(new MainActivity().getContentPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.add(labelChart);
+        frame.setVisible(true);
+
     }
 }
